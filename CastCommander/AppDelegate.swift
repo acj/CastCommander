0ba79effce55ae964631d07c7d68381040b3ed0f
Adam Jensen <acj@linuxguy.org>
2014-07-26 11:28:54 -0400

    Initial Commit

---
+++ CastCommander/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  CastCommander
+//
+//  Created by Adam Jensen on 7/26/14.
+//  Copyright (c) 2014 Adam Jensen. All rights reserved.
+//
+
+import Cocoa
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+                            
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification?) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification?) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
